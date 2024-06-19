fad62ecec6ecc24e2696defd6187561a434a6163
Deepakkk Saini <dpksaini49@gmail.com>
2024-06-19 17:45:41 +0530

    add new dir

---
+++ 11_customeHooks_currencConvert/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-center text-emerald-500 text-5xl'>Custome hooks</h1>
+    </>
+  )
+}
+
+export default App
