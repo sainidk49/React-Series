34dcdc26616d53eb5aede6547da1ac2407ceda83
Deepak Saini <dpksaini49@gmail.com>
2024-11-29 12:23:03 +0530

    remove node_modules

--- 12-React-Router-Dom/src/components/About/About.jsx
+++ 12-React-Router-Dom/src/components/About/About.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const About = () => {
     return (
         <div className="h-screen bg-slate-700 flex flex-col text-center justify-center">
             <h1 className='text-4xl text-white'>About</h1>
+            <br />
+            <Link to={"/contact"}>Contact</Link>
         </div>
+        
     );
 }
 
