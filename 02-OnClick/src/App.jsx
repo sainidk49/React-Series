59c75e8fbd6920f674b0b166fabf39c3186ba02d
Deepak Saini <dpksaini49@gmail.com>
2024-12-26 14:57:19 +0530

    check working or not

--- 02-OnClick/src/App.jsx
+++ 02-OnClick/src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   function IncreaseValue(){
-    setCount((count) => count + 1)
+    setCount((count) => count + 1);
   }
   return (
     <>
