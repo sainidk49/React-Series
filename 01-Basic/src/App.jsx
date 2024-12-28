522b67c47936d4234eede62e11ab4a709704462a
Deepak saini <dpksaini49@gmail.com>
2024-12-28 14:57:07 +0530

    remove node modules

--- 01-Basic/src/App.jsx
+++ 01-Basic/src/App.jsx
@@ -1,10 +1,21 @@
+import React, { useState, useCallback } from 'react';
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [toggle, setToggle] = useState(false);
+
+  const incrementCount = useCallback(() => {
+    setCount((prevCount) => prevCount + 1);
+  }, [count]); 
+  
   return (
-    <>
-      <h1>hello world</h1>
-    </>
-  )
-}
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={incrementCount}>Increment Count</button>
+      <button onClick={() => setToggle(!toggle)}>Toggle</button>
+      <p>{toggle ? "Toggled On" : "Toggled Off"}</p>
+    </div>
+  );
+};
 
-export default App
+export default App;
