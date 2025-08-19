9da5df5aa3c9d4182917e6aec54eaacfe41ac942
aksatthah <aks@atthah.com>
2025-08-19 14:31:59 +0530

    add custome hook

---
+++ custome-useState/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import useCustomState,{resetIndex} from './hooks/customeUseState';
+
+
+const App = () => {
+  resetIndex();
+  const [getValue, setValue] = useCustomState(0)
+  return (
+    <div className='main-container'>
+      <h1>{getValue}</h1>
+      <button onClick={() => setValue(prev => prev + 1)}>Increment</button>
+    </div>
+  );
+};
+
+export default App;
